f6b9e537e259e5442b98b908f00a111c0dd253f8
mayvastech <65177211+mayvastech@users.noreply.github.com>
2023-02-04 01:08:50 +0000

    Added onChange eventlistener to ExpenseFilter

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,15 +1,19 @@
-import './ExpensesFilter.css';
+import "./ExpenseFilter.css";
 
-const ExpensesFilter = () => {
+function ExpensesFilter() {
+    function filterChangeHandler(event) {
+        console.log(event.target.value)
+    }
+    
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+        <select onChange={filterChangeHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,35 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 
 function Expenses(props) {
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.allexpenses[0].title}
-        amount={props.allexpenses[0].amount}
-        date={props.allexpenses[0].date}
-      />
-      <ExpenseItem
-        title={props.allexpenses[1].title}
-        amount={props.allexpenses[1].amount}
-        date={props.allexpenses[1].date}
-      />
-      <ExpenseItem
-        title={props.allexpenses[2].title}
-        amount={props.allexpenses[2].amount}
-        date={props.allexpenses[2].date}
-      />
-      <ExpenseItem
-        title={props.allexpenses[3].title}
-        amount={props.allexpenses[3].amount}
-        date={props.allexpenses[3].date}
-      />
-    </Card>
+    <div>
+      <ExpensesFilter />
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.allexpenses[0].title}
+          amount={props.allexpenses[0].amount}
+          date={props.allexpenses[0].date}
+        />
+        <ExpenseItem
+          title={props.allexpenses[1].title}
+          amount={props.allexpenses[1].amount}
+          date={props.allexpenses[1].date}
+        />
+        <ExpenseItem
+          title={props.allexpenses[2].title}
+          amount={props.allexpenses[2].amount}
+          date={props.allexpenses[2].date}
+        />
+        <ExpenseItem
+          title={props.allexpenses[3].title}
+          amount={props.allexpenses[3].amount}
+          date={props.allexpenses[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 export default Expenses;
