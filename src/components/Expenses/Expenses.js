9c251d2690df5cd0deabbec6d3677dc9b01fc2b8
mayvastech <65177211+mayvastech@users.noreply.github.com>
2023-02-13 00:17:08 +0000

    rendered expense array dynamically with map method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,28 +15,11 @@ function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.allexpenses[0].title}
-          amount={props.allexpenses[0].amount}
-          date={props.allexpenses[0].date}
-        />
-        <ExpenseItem
-          title={props.allexpenses[1].title}
-          amount={props.allexpenses[1].amount}
-          date={props.allexpenses[1].date}
-        />
-        <ExpenseItem
-          title={props.allexpenses[2].title}
-          amount={props.allexpenses[2].amount}
-          date={props.allexpenses[2].date}
-        />
-        <ExpenseItem
-          title={props.allexpenses[3].title}
-          amount={props.allexpenses[3].amount}
-          date={props.allexpenses[3].date}
-        />
+        {props.allexpenses.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)}
       </Card>
     </div>
   );
 }
 export default Expenses;
+
+// map method cycles through every item in our expenses array of objects and transforms each item into ExpenseItem jsx elements in a new array.
