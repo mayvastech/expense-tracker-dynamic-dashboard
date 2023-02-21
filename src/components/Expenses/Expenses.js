05a670a2c46e35148d6a81b8b7009c6eb3aa30a7
mayvastech <65177211+mayvastech@users.noreply.github.com>
2023-02-21 17:28:19 +0000

    refactored conditional statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -21,7 +21,8 @@ function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.length === 0 ? <p>No expenses found</p> : filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />) }
+        {filteredExpenses.length === 0 && <p>No expenses found</p>}
+        {filteredExpenses.length > 0 && filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />) }
       </Card>
     </div>
   );
