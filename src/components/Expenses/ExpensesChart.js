8e914c6fd789f9e13b6405b98e5997a77c2b46bd
mayvastech <65177211+mayvastech@users.noreply.github.com>
2023-02-24 00:45:34 +0000

    Updated chartDataPoints month value to expense val

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -1,7 +1,28 @@
 import Chart from "../Chart/Chart";
 
 function ExpensesChart(props) {
-  return <Chart />;
+  const chartDataPoints = [
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "Mar", value: 0 },
+    { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
+    { label: "Jun", value: 0 },
+    { label: "Jul", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
+  ];
+
+  for (const expense of props.expenses) {
+    const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
+    // Get each month for each expense and update value of appropriate datapoint by expense amount
+    chartDataPoints[expenseMonth].value += expense.amount;
+    // increase value of a month by expense amount
+  }
+  return <Chart dataPoints={chartDataPoints} />;
 }
 
 export default ExpensesChart;
